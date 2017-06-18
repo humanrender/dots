43c3efd37de4d0a47f82a4ee044779d19385b44c
Sergio Ramirez <sergio.ramirez@cabify.com>
2017-06-18 18:26:00 +0200

    User config

--- emacs/user-config.el
+++ emacs/user-config.el
@@ -1,33 +1,44 @@
-(defun setup-indent ()
-  (setq-default indent-tabs-mode nil) ; set tabs as spaces
-  (setq-default tab-width 2) ; default tab size
-  (setq tab-width 2) ; tab size
-  (setq-default js2-basic-offset 'tab-width)
-  (setq-default css-indent-offset 'tab-width)
-  (defvaralias 'c-basic-offset 'tab-width)
-  (defvaralias 'cperl-indent-level 'tab-width)
-  (defvaralias 'js-indent-level 'tab-width)
-  (defvaralias 'css-indent-level 'tab-width)
-  (defvaralias 'css-indent-offset 'tab-width)
-  (defvaralias 'web-mode-markup-indent-offset 'tab-width)
-  (defvaralias 'web-mode-css-indent-offset 'tab-width)
-  (defvaralias 'web-mode-code-indent-offset 'tab-width)
-  (defvaralias 'web-mode-attr-indent-offset 'tab-width)
-  )
+(defun setup-indent (width)
+  (setq indent-tabs-mode nil) ; set tabs as spaces
+  (setq-default tab-width width) ; default tab size
+  (setq tab-width width) ; tab size
+  (setq js2-basic-offset width)
+  (setq css-indent-offset width)
+  (setq c-basic-offset width)
+  (setq cperl-indent-level width)
+  (setq js-indent-level width)
+  (setq css-indent-level width)
+  (setq css-indent-offset width)
+  (setq web-mode-markup-indent-offset width)
+  (setq web-mode-css-indent-offset width)
+  (setq web-mode-code-indent-offset width)
+  (setq web-mode-attr-indent-offset width)
+)
 
- (with-eval-after-load 'flycheck
-   (dolist (checker '(javascript-eslint javascript-standard))
-     (flycheck-add-mode checker 'js2-mode))
-   (add-hook 'js-mode-hook #'spacemacs//react-use-eslint-from-node-modules)
-   (spacemacs/add-flycheck-hook 'js2-mode)
-   )
+(defun dotspacemacs/user-config ()
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
 
- ;; turn on flychecking globally
- (add-hook 'after-init-hook #'global-flycheck-mode)
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
 
- (setup-indent) ; setup indentation
- (setq-default js2-show-parse-errors nil)
- (setq-default js2-strict-missing-semi-warning nil)
- (setq-default js2-strict-trailing-comma-warning nil)
- (delete-selection-mode 1)
- (global-git-commit-mode t)
+  (with-eval-after-load 'flycheck
+    (dolist (checker '(javascript-eslint javascript-standard))
+      (flycheck-add-mode checker 'js2-mode))
+    (add-hook 'js-mode-hook #'spacemacs//react-use-eslint-from-node-modules)
+    (spacemacs/add-flycheck-hook 'js2-mode)
+    )
+
+  ;; turn on flychecking globally
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  (setq-default js2-show-parse-errors nil)
+  (setq-default js2-strict-missing-semi-warning nil)
+  (setq-default js2-strict-trailing-comma-warning nil)
+  (delete-selection-mode 1)
+  (global-git-commit-mode t)
+  (setup-indent 2) ; setup indentation
+  (add-hook 'after-change-major-mode-hook (lambda()
+    (electric-indent-mode -1)
+    )) ; disable automatic indentation
+)
