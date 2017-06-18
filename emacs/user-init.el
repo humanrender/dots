f9466373fb04c25e08e2709ca25af684a6161bf6
Sergio Ramirez <sergio.ramirez@cabify.com>
2017-06-18 18:31:55 +0200

    User init

--- emacs/user-init.el
+++ emacs/user-init.el
@@ -1,6 +1,3 @@
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-(setq-default flycheck-disabled-checkers
-              (append flycheck-disabled-checkers
-                      '(javascript-jshint)))
-(setq-default git-magit-status-fullscreen t)
+(defun dotspacemacs/user-init ()
+  (setq-default git-magit-status-fullscreen t)
+)
