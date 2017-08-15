2d17804f8d036cc2f277ba4d94b9c9ab7cf8eb8e
Sergio Ramirez <sergio.ramirez@cabify.com>
2017-08-15 12:25:10 +0200

    Init configuration

---
+++ emacs/init.el
@@ -0,0 +1,17 @@
+(setq package--init-file-ensured 't)
+
+(load-file "~/.emacs.d/melpa.el") ;; melpa configuration
+(load-file "~/.emacs.d/themes.el") ;; load themes
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (solarized-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
