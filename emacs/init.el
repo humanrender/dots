3aed268e0bfa37630147e1eac765e02f08f5dc88
Sergio Ramirez <sergio.ramirez@cabify.com>
2017-08-15 12:37:47 +0200

    Changed theme to spacemacs

--- emacs/init.el
+++ emacs/init.el
@@ -8,7 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (solarized-theme))))
+ '(package-selected-packages (quote (spacemacs-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
