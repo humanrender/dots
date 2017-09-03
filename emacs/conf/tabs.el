9cceaa39f33c65e44ff8b688e6d5c221dd0a6cf6
Sergio Ramirez <sergio.ramirez@cabify.com>
2017-09-03 09:59:04 +0200

    Configure tabs

--- emacs/conf/tabs.el
+++ emacs/conf/tabs.el
@@ -1,29 +1,34 @@
-(defun setup-indent (width)
-  (setq indent-tabs-mode nil) ; set tabs as spaces
-  (setq-default tab-width width) ; default tab size
-  (setq tab-width width) ; tab size
-  (setq css-indent-offset width)
-  (setq c-basic-offset width)
-  (setq cperl-indent-level width)
-  (setq css-indent-level width)
-  (setq css-indent-offset width)
-  (setq web-mode-markup-indent-offset width)
-  (setq web-mode-css-indent-offset width)
-  (setq web-mode-code-indent-offset width)
-  (setq web-mode-attr-indent-offset width)
+(defvar w 2)
 
-  (setq js-indent-level width)
-  (setq js2-basic-offset width)
-  (setq sgml-basic-offset width)
-  (setq sgml-attribute-offset width)
+(defun setup-indent (width)
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width width) ; default tab size
+  ;; (setq css-indent-offset width)
+  ;; (setq c-basic-offset width)
+  ;; (setq cperl-indent-level width)
+  ;; (setq css-indent-level width)
+  ;; (setq css-indent-offset width)
+  ;; (setq web-mode-markup-indent-offset width)
+  ;; (setq web-mode-css-indent-offset width)
+  ;; (setq web-mode-code-indent-offset width)
+  ;; (setq web-mode-attr-indent-offset width)
+
+  ;; (setq js-indent-level width)
+  ;; (setq js2-basic-offset width)
 )
 
-(setup-indent 2) ; setup indentation
+(defun set-jsx-indentation ()
+  (setq-local sgml-basic-offset 2)
+  (setq sgml-attribute-offset 0)
+)
 
-;; for better jsx syntax-highlighting in web-mode
-;; - courtesy of Patrick @halbtuerke
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-    (let ((web-mode-enable-part-face nil))
-      ad-do-it)
-    ad-do-it))
+(setup-indent w)
+(add-hook 'js-jsx-mode-hook #'set-jsx-indentation)
+;; (add-hook 'web-mode-hook 'setup-indent)
+
+(defun global-disable-mode (mode-fn)
+  "Disable `MODE-FN' in ALL buffers."
+  (interactive "a")
+  (dolist (buffer (buffer-list))
+    (with-current-buffer buffer
+      (funcall mode-fn -1))))
--- emacs/conf/web-modes.el
+++ emacs/conf/web-modes.el
@@ -1,2 +1,22 @@
 ;; Register rjsx mode for js files under components/ folders
 (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
+
+;; (defadvice js-jsx-indent-line (after js-jsx-indent-line-after-hack activate)
+;;   "Workaround sgml-mode and follow airbnb component style."
+;;   (let* ((cur-line (buffer-substring-no-properties
+;;                     (line-beginning-position)
+;;                     (line-end-position))))
+;;     (if (string-match "^\\( +\\)\/?> *$" cur-line)
+;;       (let* ((empty-spaces (match-string 1 cur-line)))
+;;         (replace-regexp empty-spaces
+;;                         (make-string (- (length empty-spaces) sgml-basic-offset) 32)
+;;                         nil
+;;                         (line-beginning-position) (line-end-position))))))
