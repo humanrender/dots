1cf54999b718dc151c5e07f503712d0d1e932b35
Sergio Ramirez <sergio.ramirez@cabify.com>
2017-09-17 03:20:20 +0200

    helm follow persistent mode

--- emacs/conf/melpa.el
+++ emacs/conf/melpa.el
@@ -18,6 +18,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(helm-follow-mode-persistent t)
  '(package-selected-packages
    (quote
     (magit helm-ag helm-swoop helm-descbinds helm-core helm helm-projectile projectile exec-path-from-shell flycheck multiple-cursors :spacemacs-theme xclip web-mode use-package spacemacs-theme better-defaults))))
