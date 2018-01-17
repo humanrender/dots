3cda130c0ff1cb46b4ceb1d85c631f0ae759138b
Sergio Ramirez <sergio.ramirez@cabify.com>
2018-01-17 12:34:12 +0100

    Set different font-face for terminals

--- emacs/conf/multi-term.el
+++ emacs/conf/multi-term.el
@@ -1,3 +1,9 @@
+(defun multi-term-face-mode ()
+  "Change font for multi-term items."
+  (interactive)
+  (setq buffer-face-mode-face '(:family "Inconsolata" :height 150))
+  (buffer-face-mode))
+
 (use-package multi-term
   :ensure t
   :config
@@ -6,5 +12,6 @@
             (lambda ()
               (setq term-buffer-maximum-size 10000)
               (define-key term-raw-map (kbd "C-y") 'term-paste)
-            ))
+              ))
+  (add-hook 'term-mode-hook 'multi-term-face-mode)
   )
