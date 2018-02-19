c35f84ebbe361cc5be45090358db3283cb63ded4
Sergio Ramirez <sergio.ramirez@cabify.com>
2018-02-19 12:08:05 +0100

    Add prettier to the index

--- emacs/init.el
+++ emacs/init.el
@@ -14,5 +14,5 @@
 (load-file "~/.emacs.d/conf/mouse.el") ;; mouse conf for xterm
 (load-file "~/.emacs.d/conf/indium.el") ;; js debugging
 (load-file "~/.emacs.d/conf/multi-term.el") ;; terminals inside emacs
-;; (load-file "~/.emacs.d/conf/prettier.el") ;; terminals inside emacs
+(load-file "~/.emacs.d/conf/prettier.el") ;; terminals inside emacs
 
